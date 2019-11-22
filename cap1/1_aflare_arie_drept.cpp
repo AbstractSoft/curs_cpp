662cff76eae0da16dd1e0b4018139fcbbf28b294
Eduard Ghergu <ghergu@yahoo.com>
2019-11-22 12:37:10 +0200

    Initial  setup

---
+++ cap1/1_aflare_arie_drept.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    double L, l;
+    std::cout << "Lungime=";
+    std::cin >> L;
+    std::cout << "Latime=";
+    std::cin >> l;
+
+    double aria = L * l;
+    std::cout << "Aria=" << aria;
+
+    return 0;
+}
